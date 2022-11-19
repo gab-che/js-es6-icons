e67979da538fb3b5cac95589ff5048f5decb5a74
gab-che <gabriellachessa1@gmail.com>
2022-11-19 17:23:51 +0100

    js adds cards to html

--- js/script.js
+++ js/script.js
@@ -2,7 +2,16 @@
 const row = document.querySelector(".row");
 
 iconArray.forEach((element)=>{
-    Object.keys(element).forEach(()=>{element.family = "solid";})
-})
+    Object.keys(element).forEach(()=>{element.family = "solid";});
 
-console.log(iconArray)
+    const col = document.createElement("div");
+    col.classList.add("col");
+    
+    const card = document.createElement("div");
+    card.classList.add("card", "card_style");
+    card.innerHTML = `<i class="${element.prefix}${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+    <h5>${element.name}</h5>`
+    col.append(card);
+    
+    row.append(col);
+})
