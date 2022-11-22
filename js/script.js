2fcc5c91ab9f8bc9d6e482f783c3056055bca388
gab-che <gabriellachessa1@gmail.com>
2022-11-22 18:46:40 +0100

    adds working select menu

--- js/script.js
+++ js/script.js
@@ -8,20 +8,53 @@ iconArray.forEach((element)=>{
 
     const col = document.createElement("div");
     col.classList.add("col");
-    
+
     const card = document.createElement("div");
     card.classList.add("card", "card_style");
     card.innerHTML = `<i class="${element.prefix}${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
     <h5>${element.name}</h5>`
     col.append(card);
-    
+    col.classList.add(element.type);
+
     row.append(col);
 })
 
-const typesList = iconArray.reduce((p, c) =>{
-    p[c.type] = p[c.type] || [];
-    p[c.type].push(c);
-    return p;
-}, []);
+const typeList = [];
 
-//select.append(selectType);
+iconArray.forEach(element=>{
+
+    if (!typeList.includes(element.type)) {
+        typeList.push(element.type);
+    }
+
+});
+
+typeList.push("all");
+typeList.sort();
+typeList.forEach((element)=>{
+    const option = document.createElement("option");
+    option.value = element;
+    option.innerText = element;
+    selectType.append(option);
+})
+
+select.append(selectType);
+
+const allCols = document.querySelectorAll(".col");
+selectType.addEventListener('change', function() {
+
+    if(this.value === "all"){
+        allCols.forEach((element)=>{
+            element.classList.remove("d-none")
+        });
+    } else {
+        
+        allCols.forEach(element=>{
+            if (element.classList.contains(this.value)){
+                element.classList.remove("d-none");
+            } else{
+                element.classList.add("d-none");
+            }
+        });
+    }
+});
